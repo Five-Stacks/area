b5432871f3e6513b9c44edf8ceb174ffe5283ae2
ArthuryanLoheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-09-23 18:00:30 +0200

    ✨ Update sign-in page layout with text field components; add new styles and Angular dependencies

--- web/src/app/components/Forms/text-field-component/text-field-component.ts
+++ web/src/app/components/Forms/text-field-component/text-field-component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-text-field-component',
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './text-field-component.html',
-  styleUrl: './text-field-component.css'
+  styleUrl: './text-field-component.css',
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' }
+    }
+  ]
 })
 export class TextFieldComponent {
-
+  @Input() label = 'Input';
 }
--- web/src/app/pages/AuthPages/sign-in-page/sign-in-page.ts
+++ web/src/app/pages/AuthPages/sign-in-page/sign-in-page.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonFullComponent } from '../../../components/Buttons/button-component-full/button-component-full';
+import { TextFieldComponent } from '../../../components/Forms/text-field-component/text-field-component';
 
 @Component({
   selector: 'app-sign-in-page',
-  imports: [ButtonFullComponent],
+  imports: [ButtonFullComponent, TextFieldComponent],
   templateUrl: './sign-in-page.html',
   styleUrl: './sign-in-page.css'
 })
