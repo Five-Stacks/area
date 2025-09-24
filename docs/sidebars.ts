de0b0d5f6822740dab7bfd3a421420ec8ce78c17
Jean BIZEUL <146715844+JeanBizeul@users.noreply.github.com>
2025-09-24 14:40:22 +0200

    📝 Setup Docusaurus
    * 📝 Add default typescript docusaurus website under the docs folder
    
    * 🔥 Remove files not used in docs only Docusaurus websites
    
    * 👷 Add the necessary configuration in the docusaurus config file to make it update the gh-pages branch website.
    
    * 👷 Add workflow to deploy the documentation on GitHub pages on push to the dev branch
    
    * 🍱 Use new logo instead of default docusaurus one
    
    * 📝🏗️ Setup production ready documentation architecture with getting started, guides & API references
    
    * 📝 Add welcome page to the documentation
    
    * ♻️ 📝 Use the sidebar.ts config file to set the layout of pages in the doc instead of the individual position markers in each docs header.
    
    * 📝 Add contributing.md to the new doc
    
    * 📝 Add development workflow part in the contributing page
    Include the old branching & pull requests startegy
    Include the commit norm
    
    * 📝 Add architecture overview in the getting-started part of the documentation
    
    * 📝 Add installation guide
    
    * 📝 Merge all pull request templates into one
    GitHub doesn't actually suports multiple PR templates very well.
    
    * 📝 🚚 Move contribution guide to the guides directory
    was in the getting-started directory before
    
    * 🔀 Set base for pr from main to dev

---
+++ docs/sidebars.ts
@@ -0,0 +1,42 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  gettingStartedSidebar: [
+    {
+      type: 'category',
+      label: '🚀 Getting Started',
+      collapsible: false,
+      items: [
+        'getting-started/welcome',
+        'getting-started/architecture',
+      ],
+    },
+  ],
+
+  guidesSidebar: [
+    {
+      type: 'category',
+      label: '📚 Guides',
+      collapsible: false,
+      items: [
+        'guides/installation',
+        'guides/contributing',
+        // 'guides/deployment',
+      ],
+    },
+  ],
+
+  apiSidebar: [
+    {
+      type: 'category',
+      label: '🛠 API Reference',
+      collapsible: false,
+      items: [
+        'api/overview',
+        // 'api/endpoints',
+      ],
+    },
+  ],
+};
+
+export default sidebars;
