0f1cd59239700cd6e324b33e673fa0fec8c660d8
JeanBizeul <jean.bizeul@epitech.eu>
2025-09-30 15:25:50 +0200

    ✨ Add fonts & load them on app launch

--- mobile/app/(auth)/landing-page.tsx
+++ mobile/app/(auth)/landing-page.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { globalTextStyle } from "@/src/styles/global";
 
 export default function Index() {
   return (
@@ -11,7 +12,11 @@ export default function Index() {
       }}
     >
       <Text>Landing page</Text>
-      <Link href={"/login"} style={{color: "blue"}}>GOTO login</Link>
+      <Link href={"/login"} style={globalTextStyle.thin}>THIN text</Link>
+      <Link href={"/login"} style={globalTextStyle.regular}>REGULAR text</Link>
+      <Link href={"/login"} style={globalTextStyle.medium}>MEDIUM text</Link>
+      <Link href={"/login"} style={globalTextStyle.bold}>BOLD text</Link>
+      <Link href={"/login"} style={globalTextStyle.regular}>GOTO login</Link>
     </View>
   );
 }
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
+import {useFonts} from "expo-font";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto loading before all elements are loaded
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  var isConnected:boolean = true;
+  var isConnected:boolean = false;
+
+  const [fontsLoaded] = useFonts({
+    "Inter-Thin": require("@/assets/fonts/Inter-Thin.ttf"),
+    "Inter-Regular": require("@/assets/fonts/Inter-Regular.ttf"),
+    "Inter-Medium": require("@/assets/fonts/Inter-Medium.ttf"),
+    "Inter-Bold": require("@/assets/fonts/Inter-Bold.ttf")
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hide();
+    }
+  }, [fontsLoaded]);
+
   return <Stack>
     {isConnected ?
       (<Stack.Screen name="(tabs)" />)
