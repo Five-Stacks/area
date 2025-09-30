baa0ce3bd3500677976f81fc7602f83d237fc9a9
ArthuryanLoheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-09-30 10:54:52 +0200

    ✨ Add valueChange output event to options field component

--- web/src/app/components/Forms/options-field-component/options-field-component.ts
+++ web/src/app/components/Forms/options-field-component/options-field-component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-options-field-component',
@@ -11,8 +11,10 @@ export class OptionsFieldComponent {
   @Input() options: string[] = [];
   @Input() label: string = '';
   @Input() selectedOption: string = '';
+  @Output() valueChange = new EventEmitter<string>();
 
   onOptionChange(event: any) {
     this.selectedOption = event.target.value;
+    this.valueChange.emit(this.selectedOption);
   }
 }
