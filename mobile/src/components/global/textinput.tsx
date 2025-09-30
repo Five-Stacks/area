2c176a93f3b449aabcc3d135e4454d17cf954945
nathan bezard <nathan.bezard@epitech.eu>
2025-09-30 15:48:21 +0200

    ✨ Add textinput component

---
+++ mobile/src/components/global/textinput.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet , TextInput, TouchableOpacity} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+<Ionicons name="eye" size={20} color="gray" />
+
+
+type InputProps = {
+    onChangeText: (text: string) => void;
+    value?: string;
+    placeholder?: string;
+    secureTextEntry?: boolean;
+}
+
+
+const Input = ({ value, onChangeText, placeholder, secureTextEntry }: InputProps) => {
+    const { inputStyle, containerStyle } = styles;
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    return (
+        <View style={containerStyle}>
+            <TextInput
+                style={inputStyle}
+                value={value}
+                onChangeText={onChangeText}
+                placeholder={placeholder}
+                secureTextEntry={secureTextEntry && !isPasswordVisible}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+
+        {secureTextEntry && (
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+        >
+          <Ionicons
+            name={isPasswordVisible ? "eye" : "eye-off"}
+            size={20}
+            color="gray"
+          />
+        </TouchableOpacity>
+      )}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inputStyle: {
+        color: '#000',
+        fontSize: 15,
+        margin: 15,
+        height: 40,
+        width: 250,
+        borderColor: '#ddd',
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    containerStyle: {
+        height: 60,
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    iconContainer: {
+        position: "absolute",
+        right: 25,
+        top: 25,
+    },
+});
+
+export default Input;
