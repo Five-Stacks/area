960b8fbe5f739ee6838eb72d8674bc58770e0f8a
nathan bezard <nathan.bezard@epitech.eu>
2025-10-01 17:18:46 +0200

    ✨ Add separator component

---
+++ mobile/src/components/auth/login/separator.tsx
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const OrDivider = () => (
+  <View style={styles.container}>
+    <View style={styles.line} />
+    <Text style={styles.text}>OR</Text>
+    <View style={styles.line} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 16,
+  },
+  line: {
+    width: '40%',
+    height: 1,
+    backgroundColor: "#ccc", // or any color you want
+  },
+  text: {
+    marginHorizontal: 8,
+    color: "#888",
+    fontWeight: "bold",
+  },
+});
+
+export default OrDivider;
