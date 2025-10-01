48018039ba9cde525ae0acc918905803ef15a2fa
JeanBizeul <jean.bizeul@epitech.eu>
2025-10-01 17:45:46 +0200

    🔥 Remove all placeholder components from the Dashboard

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -1,9 +1,4 @@
-import { Text, View } from "react-native";
-import Dropdown from "@/src/components/global/dropdown";
-import Input from "@/src/components/global/textinput";
-import Checkbox from "@/src/components/global/checkmark";
-import StylizedButton from "@/src/components/global/button";
-import Toggle from "@/src/components/global/toggle";
+import { View } from "react-native";
 
 export default function Index() {
   return (
@@ -13,24 +8,6 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Text>Dashboard screen</Text>
-      <Dropdown
-        options={["Option 1", "Option 2", "Option 3"]}
-        placeholder="Select an option"
-      />
-      <Input placeholder="Enter text" secureTextEntry={true} />
-      <Input placeholder="Password" secureTextEntry={false} />
-      <Checkbox label="Accept Terms and Conditions" />
-      <StylizedButton
-        label="Press Me"
-        onPress={() => alert("Button Pressed!")}
-      />
-      <Toggle
-        width={60}
-        value={false}
-        onValueChange={(val) => console.log("Toggle is now:", val)}
-      />
-    </View>
+    />
   );
 }
