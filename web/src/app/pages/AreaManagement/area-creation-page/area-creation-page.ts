7b5556433dc5811e92bdf3ed7214317d9b7f1e21
ArthuryanLoheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-10-01 17:51:12 +0200

    ✨ Update area creation page styles and enhance SVG icons for improved UI/UX

--- web/src/app/pages/AreaManagement/area-creation-page/area-creation-page.ts
+++ web/src/app/pages/AreaManagement/area-creation-page/area-creation-page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderDashBoardComponent } from '../../../components/Headers/header-component-dashboard/header-component-dashboard';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-area-creation-page',
-  imports: [HeaderDashBoardComponent, CommonModule],
+  imports: [HeaderDashBoardComponent, CommonModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './area-creation-page.html',
   styleUrl: './area-creation-page.css'
 })
@@ -12,6 +23,17 @@ export class AreaCreationPage {
   isEditing: boolean = false;
   idEditing: number = -1; // 1, 2, ... for actions
   nameArea: string = '';
+
+  step: number = 1;
+
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
   area : {
     name?: string;
@@ -33,13 +55,23 @@ export class AreaCreationPage {
     actions: [{id: 1}],
   };
 
-  editTrigger(areaId: number) {
-    if (this.isEditing && this.idEditing === areaId) {
+  editAction(actionId: number) {
+    if (this.isEditing && this.idEditing === actionId) {
       this.isEditing = false;
       this.idEditing = -1;
     } else {
       this.isEditing = true;
-      this.idEditing = areaId;
+      this.idEditing = actionId;
+    }
+  }
+
+  editTrigger(triggerId: number) {
+    if (this.isEditing && this.idEditing === triggerId) {
+      this.isEditing = false;
+      this.idEditing = -1;
+    } else {
+      this.isEditing = true;
+      this.idEditing = triggerId;
     }
   }
 
