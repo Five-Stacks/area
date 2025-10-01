a034932a4b28c82d759027c611632d7af25cc456
mathys-f <mathys.fillion@epitech.eu>
2025-10-01 16:44:25 +0200

    ✨ Add route definitions for action, reaction, area, and areaExecution

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import corsSetup from './config/cors.js';
 import authRouter from './routes/authRoute.js';
+import actionRouter from './routes/actionRoute.js';
+import reactionRouter from './routes/reactionRoute.js';
+import areaRouter from './routes/areaRoute.js';
+import areaExecutionRouter from './routes/areaExecutionRoute.js';
 import notFound from './middleware/notFound.js';
 
 /* App initialization */
@@ -18,6 +22,10 @@ app.use(cookieParser());
 
 /* Define routes */
 app.use('/api/auth', authRouter);
+app.use('/api/action', actionRouter);
+app.use('/api/reaction', reactionRouter);
+app.use('/api/area', areaRouter);
+app.use('/api/areaExecution', areaExecutionRouter);
 
 /* 404 Middleware */
 app.use(notFound);
