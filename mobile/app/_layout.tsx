2c29e854a1b073957c5dc7deadff3dbf7ac9db91
JeanBizeul <jean.bizeul@epitech.eu>
2025-10-01 10:19:54 +0200

    🐛 Rename main pages of folders to index.tsx to allow the app to work under IOS dominion

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack, SplashScreen } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import "react-native-screens";
 
 // Prevent the splash screen from auto loading before all elements are loaded
 SplashScreen.preventAutoHideAsync();
@@ -8,7 +9,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   let isConnected: boolean = false;
 
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, error] = useFonts({
     "Inter-Thin": require("@/assets/fonts/Inter-Thin.ttf"),
     "Inter-Regular": require("@/assets/fonts/Inter-Regular.ttf"),
     "Inter-Medium": require("@/assets/fonts/Inter-Medium.ttf"),
@@ -17,9 +18,15 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (fontsLoaded) {
-      SplashScreen.hide();
+      SplashScreen.hideAsync();
+      console.log("Fonts loaded, splashscreen hidden !");
     }
   }, [fontsLoaded]);
+
+  useEffect(() => {
+    if (error)
+      console.error("Problem occurend while loading fonts:" + error.message);
+  }, [error]);
 
   return (
     <Stack>
