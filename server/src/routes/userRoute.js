3bc2ef4090ec67d67a2b4444e3dfff49d2204bda
Léopold PINTARD <146716915+Leopold-PINTARD@users.noreply.github.com>
2025-10-02 09:48:29 +0200

    🐛 Fix import path for verifyToken middleware in user routes

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
-import verifyToken from '../middleware/authMiddleware.js';
+import verifyToken from '../middleware/verifyToken.js';
 
 /* Initialize router */
 const router = express.Router();
 
 /* Define CRUD routes for users */
+router.post('/', verifyToken, userController.createUser);
 router.get('/', verifyToken, userController.getAllUsers);
 router.get('/:id', verifyToken, userController.getUserById);
 router.put('/:id', verifyToken, userController.updateUser);
