126897ca5887b6928db60818428d98cfa4774d2f
JeanBizeul <jean.bizeul@epitech.eu>
2025-10-08 04:40:44 +0200

    ✨ Add api function to fetch actions

---
+++ mobile/src/api/action.ts
@@ -0,0 +1,28 @@
+import Action from "@/src/types/action";
+import API_BASE_URL from "./serverAdress";
+
+export async function getActions(): Promise<Action[]> {
+  const res = await fetch(`${API_BASE_URL}/action`);
+  if (!res.ok) throw new Error("Failed to fetch actions");
+  const json = await res.json();
+  return json.data;
+}
+
+export async function getActionById(id: string): Promise<Action> {
+  const res = await fetch(`${API_BASE_URL}/action/${id}`);
+  if (!res.ok) {
+    if (res.status === 404) throw new Error("Action not found");
+    throw new Error("Failed to fetch action");
+  }
+  const json = await res.json();
+  return json.data;
+}
+
+export async function getActionsByServiceId(
+  serviceId: string,
+): Promise<Action[]> {
+  const res = await fetch(`${API_BASE_URL}/action/service/${serviceId}`);
+  if (!res.ok) throw new Error("Failed to fetch actions by service ID");
+  const json = await res.json();
+  return json.data;
+}
