a640806a3e0dec4d9ec7e7a53681f535bae23da6
ArthuryanLoheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-10-17 14:46:00 +0200

    ✨ Refactor code structure for improved readability and maintainability

--- web/src/app/pages/explorer-page/explorer-page.ts
+++ web/src/app/pages/explorer-page/explorer-page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/Headers/header-component/header-component';
+import { TextFieldComponent } from '../../components/Forms/text-field-component/text-field-component';
+import { OptionsFieldComponent } from '../../components/Forms/options-field-component/options-field-component';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-explorer-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TextFieldComponent, OptionsFieldComponent, CommonModule],
   templateUrl: './explorer-page.html',
   styleUrl: './explorer-page.css'
 })
-export class ExplorerPage {
+export class ExplorerPage implements OnInit {
+  private apiService = inject(ApiService);
 
+  searchService = '';
+  listServices : {
+    name: string;
+    id: number;
+    description: string;
+  }[] = [];
+
+  filteredSerice() {
+    return this.listServices.filter(service =>
+      service.name.toLowerCase().includes(this.searchService.toLowerCase())
+    );
+  }
+
+  ngOnInit(): void {
+    this.apiService.get<any>('service').subscribe({
+      next: (data) => {
+        this.listServices = data.data;
+      },
+      error: (err) => {
+        console.error('Error fetching services:', err);
+      }
+    });
+  }
 }
