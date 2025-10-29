92ffa47313a5b4eec0c9b8983820222b8f9ad987
nathan bezard <nathan.bezard@epitech.eu>
2025-10-29 19:33:10 +0100

    ✨ Pretty landing page

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,18 +1,89 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet, ImageBackground } from "react-native";
 import { router } from "expo-router";
 import StylizedButton from "@/src/components/global/button";
+import * as SecureStore from "expo-secure-store";
+import { useEffect } from "react";
+import { Button } from "@react-navigation/elements";
 
 export default function Index() {
+//  useEffect(() => {
+//    async function checkToken() {
+//      const token = await SecureStore.getItemAsync("token");
+//      if (token) {
+//        router.replace("/(tabs)/dashboard");
+//      }
+//    }
+//    checkToken();
+//  }, []); 
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Landing page</Text>
-      <StylizedButton label="Log In" onPress={() => router.push("/login")} />
+      style={styles.view}>
+      <ImageBackground
+        source={require("@/assets/images/BgPoints.png")}
+        style={styles.backgroundContainer}
+        imageStyle={styles.backgroundImage}
+      />
+      <Image
+        source={require("@/assets/images/LogoVertical.png")}
+        style={styles.image}
+      />
+      <Text style={styles.title1} >Automation for</Text>
+      <Text style={styles.title2} >business and home</Text>
+      <Text style={styles.text}>Save time and get more done</Text>
+      <View
+        style={styles.buttonContainer}
+      >
+        <StylizedButton 
+          label="START TODAY"
+          onPress={() => router.push("/login")}
+          style={styles.button} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  backgroundContainer: {
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+  },
+  backgroundImage: {
+    resizeMode: 'repeat',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 100,
+    marginBottom: 80,
+  },
+  title1: {
+    fontWeight: "bold",
+    fontSize: 35,
+    paddingTop: 10,
+  },
+  title2: {
+    fontWeight: "bold",
+    fontSize: 35,
+    paddingBottom: 20,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    width: "100%",
+    height: "20%",
+    position: "absolute",
+    bottom: 0,
+  },
+  button: {
+    alignSelf: 'center',
+  }
+});
