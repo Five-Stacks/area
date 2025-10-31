b4410906c3c11b7770d4ffb221363baf6ac23afa
mathys-f <mathys.fillion@epitech.eu>
2025-10-31 05:09:41 +0100

    🔥 Remove unused HttpClientTestingModule import from area-creation-page.spec.ts

--- web/src/app/pages/AreaManagement/area-creation-page/area-creation-page.spec.ts
+++ web/src/app/pages/AreaManagement/area-creation-page/area-creation-page.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AreaCreationPage } from './area-creation-page';
 import { ApiService } from '../../../services/api.service';
