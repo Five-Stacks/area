c850d30846d8f18844a06344deec87d0a2ea10eb
mathys-f <mathys.fillion@epitech.eu>
2025-10-31 04:33:42 +0100

    ✨ Add GitHub reactions for issues, pull requests, stars, and releases

--- server/src/services/reactions/index.js
+++ server/src/services/reactions/index.js
@@ -2,12 +2,20 @@
 import * as timer from "./timer/timer.js";
 import * as googleSendEmail from "./google/sendEmail.js";
 import * as googleCreateNewEvent from "./google/createNewEvent.js";
+import * as githubCreateIssue from "./github/createIssue.js";
+import * as githubCreatePullRequest from "./github/createPullRequest.js";
+import * as githubStarRepository from "./github/starRepository.js";
+import * as githubCreateRelease from "./github/createRelease.js";
 
 /* Reactions handler */
 const reactionsHandler = {
     'Timer': timer.default ?? timer,
     'Send email with Gmail': googleSendEmail.default ?? googleSendEmail,
-    'Create Google Calendar event': googleCreateNewEvent.default ?? googleCreateNewEvent
+    'Create Google Calendar event': googleCreateNewEvent.default ?? googleCreateNewEvent,
+    'Create GitHub Issue': githubCreateIssue.default ?? githubCreateIssue,
+    'Create GitHub Pull Request': githubCreatePullRequest.default ?? githubCreatePullRequest,
+    'Star GitHub Repository': githubStarRepository.default ?? githubStarRepository,
+    'Create GitHub Release': githubCreateRelease.default ?? githubCreateRelease,
 };
 
 /* Export reactions handler */
